088c820105134b84b9b7dbe54ebb61549411d436
Michael032194 <jheck000021@gmail.com>
2025-06-07 07:40:02 +0000

    Chick to Chicken

--- chapter1/Constructor1Lesson.java
+++ chapter1/Constructor1Lesson.java
@@ -2,21 +2,19 @@ package chapter1;
 
 public class Constructor1Lesson {
     public static void main(String[] args) {
-            Chick c1 = new Chick();
-            Chick c2 = new Chick();
-            c2.name = "wakikik";
+            Chicken c1 = new Chicken();
+            Chicken c2 = new Chicken();
             System.out.println("end of program");
     }
 
 }
 
-class Chick{
+class Chicken{
+    int numEggs = 0;
     String name;
-    public Chick(){
-        System.out.println("creating new Chick Object");
-        this.name = "wakokok";
+    public Chicken(){
+        name = "Duke";
     }
-    public void  Chick(){
-        System.out.println("this is a method");
+  
     }
-}
+
